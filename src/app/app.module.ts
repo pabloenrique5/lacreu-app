de7dcd861d16fd70cbeb68f0067398843e7d6a50
Pablo <penriquefernandez@gmail.com>
2020-08-29 17:40:25 +0200

    Añado Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { firebaseConfig } from 'src/environments/environment';
+
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+            AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,17 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAxt9WtdZuDc9fvRclyVGU4CJhhTi5lE-M',
+  authDomain: 'la-creu.firebaseapp.com',
+  databaseURL: 'https://la-creu.firebaseio.com',
+  projectId: 'la-creu',
+  storageBucket: 'la-creu.appspot.com',
+  messagingSenderId: '303918922902',
+  appId: '1:303918922902:web:33f83e587a2759528316b7',
+  measurementId: 'G-XXPHYHQPQ9'
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
