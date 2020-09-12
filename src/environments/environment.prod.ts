986b1b1ff173e49766b6aed36483cd8f3fb6d40c
Pablo <penriquefernandez@gmail.com>
2020-09-12 18:23:57 +0200

    Generación de iconos, cambios en la configuración

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAxt9WtdZuDc9fvRclyVGU4CJhhTi5lE-M',
+  authDomain: 'la-creu.firebaseapp.com',
+  databaseURL: 'https://la-creu.firebaseio.com',
+  projectId: 'la-creu',
+  storageBucket: 'la-creu.appspot.com',
+  messagingSenderId: '303918922902',
+  appId: '1:303918922902:web:33f83e587a2759528316b7',
+  measurementId: 'G-XXPHYHQPQ9'
+};
